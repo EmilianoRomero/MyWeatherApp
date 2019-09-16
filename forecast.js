f13e231311c5aedae6edca26f54f57baf8953e15
Emiliano Romero <emiliano.romero@gmx.de>
2019-09-16 14:50:39 +0200

    Applying some Object Oriented JavaScript
    - method "Forecast" created along with its properties and methods
    - the keys are stored on the class and called by "this."
    - all the separate functions are bundled as async methods inside the Forecast class, and we set a constructor to set the properties on that class
    - a new instance of that class is created with its methods and properties

--- forecast.js
+++ forecast.js
@@ -1,38 +1,27 @@
-//WORKING WITH THE APIs
-//API key for the AccuWeather Site
-const key = "nVXAnrLGxpPF2AIZArGPevsnabun6gIh";
-
-//getting the weather info with an async function (we're using the json!)
-const getWeather = async (id) => {
-
-    const base = "http://dataservice.accuweather.com/currentconditions/v1/"
-    const query = `${id}?apikey=${key}`;
-
-    //fetching the results
-    const response = await fetch(base + query);
-    const data = await response.json();
-
-    /*
-    console.log(data[0]);
-    console.log(data[0].Temperature);
-    console.log(data[0].Temperature.Metric.Value);
-    */
-    return data[0];
-    
-};
-
-
-//getting the info for the city with the Key code
-const getCity = async (city) => {
-
-    const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
-    const query = `?apikey=${key}&q=${city}`;
-
-    const response = await fetch(base + query);
-    const data = await response.json();
-
-    //console.log(data[0]);
-    //gets the closes match
-    return data[0];
-
-};
+class Forecast {
+    constructor() {
+        this.key = "nVXAnrLGxpPF2AIZArGPevsnabun6gIh";
+        this.weatherURI = "http://dataservice.accuweather.com/currentconditions/v1/";
+        this.cityURI = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    }
+    async updateCity(city) {
+        const cityDets = await this.getCity(city);
+        const weather = await this.getWeather(cityDets.Key);
+        return {
+            cityDets,
+            weather
+        };
+    }
+    async getCity(city) {
+        const query = `?apikey=${this.key}&q=${city}`;
+        const response = await fetch(this.cityURI + query);
+        const data = await response.json();
+        return data[0];
+    }
+    async getWeather(id) {
+        const query = `${id}?apikey=${this.key}`;
+        const response = await fetch(this.weatherURI + query);
+        const data = await response.json();
+        return data[0];
+    }
+}
--- weather.js
+++ weather.js
@@ -4,24 +4,17 @@ const card = document.querySelector(".card");
 const details = document.querySelector(".details");
 const time = document.querySelector("img.time"); //IsDayTime
 const icon = document.querySelector(".icon img"); //WeatherIcon
+const forecast = new Forecast(); //calling up the Forecast Object along with its meths and props
 
 const updateUI = (data) => {
-    /* I'M NOT USING THIS ANYMORE BECAUSE I'M DESTRUCTURING...
-    const cityDets = data.cityDets;
-    const weather = data.weather;
-    */
-    //Destructuring properties: I want to get the properties from this object and store them
-    //in constants with the same name
     const {
         cityDets,
         weather
     } = data;
 
-    //update time conditions icon
     const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
     icon.setAttribute("src", iconSrc);
 
-    //updating the details templates with this info
     details.innerHTML = `
         <h5 class="my-3">${cityDets.EnglishName}</h5>
         <div class="my-3">${weather.WeatherText}</div>
@@ -30,59 +23,29 @@ const updateUI = (data) => {
             <span>&deg;C</span>
         </div>
     `;
-    //Day/Night bckg images
-    /*
-    let timeSrc = null;
-    if(weather.IsDayTime){
-        timeSrc = "img/day.svg";
-    } else {
-        timeSrc = "img/night.svg";
-    }
-    time.setAttribute("src", timeSrc);
-    */
-    //THE SAME EXPRESSED WITH THE TERNARY OPERATOR
+
     let timeSrc = weather.IsDayTime ? "img/day.svg" : "img/night.svg";
     time.setAttribute("src", timeSrc);
 
-    //making it visible by removing original class d-none
     if (card.classList.contains("d-none")) {
         card.classList.remove("d-none");
     }
 };
 
-const updateCity = async (city) => {
-    const cityDets = await getCity(city);
-    const weather = await getWeather(cityDets.Key);
-    //console.log(cityDets);
-    //Applying OSN (Object Shorthand Notation)
-    return {
-        cityDets,
-        weather
-    };
-};
-
 cityForm.addEventListener("submit", e => {
-    //always prevent refresh by default!
     e.preventDefault();
-    //getting the value of the input, avoiding empty spaces
     const city = cityForm.city.value.trim();
-    //cleaning the form
     cityForm.reset();
 
-    //update the ui with the new city
-    updateCity(city)
+    forecast.updateCity(city)
         .then(data => updateUI(data))
         .catch(err => console.log(err));
 
-    //Applying the recently learnt localStorage method
-    //Storing the last search as the first appearing "face" of the app
     localStorage.setItem("city", city);
 });
 
-// if check: if the city exists in local storage ("truthy" yes, "falsy" no)
-// it makes an automatic call to get the city updating the local storage
 if (localStorage.getItem("city")) {
-    updateCity(localStorage.getItem("city")) //this returns a promise
-        .then(data => updateUI(data))       //take the data and update the UI
-        .catch(err => console.log(err));    //catching possible errors as usual
+    forecast.updateCity(localStorage.getItem("city"))
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
 }
